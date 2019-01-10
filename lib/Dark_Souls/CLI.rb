1d4d9abdfbbea4a29463a226be9e296d9bf82685
Dakota Clark <bannaray95@gmail.com>
2019-01-10 02:41:16 +0000

    added most of start

--- lib/Dark_Souls/CLI.rb
+++ lib/Dark_Souls/CLI.rb
@@ -6,11 +6,20 @@ Class DarkSouls::CLI
   end
 
   def start
+    puts ""
     puts "choose a class, Warrior, Knight, Wanderer, Thief, Bandit, Hunter, Sorcerer, Pyromancer, Cleric, Deprived."
-    input = 
+    input = gets.strip
+    case input
+    when "Warrior"
+      puts "you have chosen Warrior..."
+    when "Knight"
+      puts "you have chosen Knight..."
+    when "Wanderer"
+      puts "you have chosen Wanderer..."
+    when "Thief"
+      puts "you have chosen Bandit..."
+    
   end
   
-  def choose_class
-    
   
 end
