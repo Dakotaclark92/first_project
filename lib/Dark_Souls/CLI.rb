5eead36cad8da3842e317f69f9d41fb8efbe4dff
Dakota Clark <bannaray95@gmail.com>
2019-01-10 01:28:48 +0000

    saving so I don't loose any data

--- lib/Dark_Souls/CLI.rb
+++ lib/Dark_Souls/CLI.rb
@@ -2,6 +2,15 @@ Class DarkSouls::CLI
 
   def call
     puts "welcome to Dark Souls"
+    start
   end
 
+  def start
+    puts "choose a class, Warrior, Knight, Wanderer, Thief, Bandit, Hunter, Sorcerer, Pyromancer, Cleric, Deprived."
+    input = 
+  end
+  
+  def choose_class
+    
+  
 end
