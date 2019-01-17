8220616df84a65742006ae97b3fde5446d5b40f3
Dakota Clark <bannaray95@gmail.com>
2019-01-17 01:04:08 +0000

    added class.rb depth

--- lib/Dark_Souls/class.rb
+++ lib/Dark_Souls/class.rb
@@ -1,6 +1,6 @@
 class DarkSouls::Classes
   
-  def Classes
+  def self.all
     puts <<-DOC.gsub /^\s*/,''
       1. Warrior
       2. Knight
@@ -13,6 +13,61 @@ class DarkSouls::Classes
       9. Cleric
       10. Deprived
     DOC
+    
+    class_1 = self.new
+    class_1.name = 
+    class_1.description = 
+    class_1.starting_equipment = 
+    
+    class_2 = self.new
+    class_2.name = 
+    class_2.description = 
+    class_2.starting_equipment = 
+    
+    class_3 = self.new
+    class_3.name = 
+    class_3.description = 
+    class_3.starting_equipment =
+    
+    class_4 = self.new
+    class_4.name = 
+    class_4.description = 
+    class_4.starting_equipment =
+    
+    class_4 = self.new
+    class_4.name = 
+    class_4.description = 
+    class_4.starting_equipment =
+    
+    class_5 = self.new
+    class_5.name = 
+    class_5.description = 
+    class_5.starting_equipment =
+    
+    class_6 = self.new
+    class_6.name = 
+    class_6.description = 
+    class_6.starting_equipment =
+    
+    class_7 = self.new
+    class_7.name = 
+    class_7.description = 
+    class_7.starting_equipment = 
+    
+    class_8 = self.new
+    class_8.name = 
+    class_8.description = 
+    class_8.starting_equipment =
+    
+    class_9 = self.new
+    class_9.name = 
+    class_9.description = 
+    class_9.starting_equipment =
+    
+    class_10 = self.new
+    class_10.name = 
+    class_10.description = 
+    class_10.starting_equipment =
     
   end
 
