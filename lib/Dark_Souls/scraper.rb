48e7f1882a82e15d94d3e1f4d74fd29bc5afabdd
Dakota Clark <bannaray95@gmail.com>
2019-01-18 18:21:29 +0000

    put in the individual scraped Des. and starting equ. code)

--- lib/Dark_Souls/scraper.rb
+++ lib/Dark_Souls/scraper.rb
@@ -14,8 +14,8 @@ class Scraper
   end
   
 #name = doc.css("h3 .mw-headline").map{|n|n.text}
-#des = doc.css(".floatright + p").map{|n|n.text}
-#starting equ = doc.css("p + ul").map{|n|n.text}
+#des = doc.css(".floatright + p").map{|n|n.text}[0...10]
+#starting equ = doc.css("p + ul").map{|n|n.text}[0...10]
   
   (0..9).each{|n|puts n}
 end
